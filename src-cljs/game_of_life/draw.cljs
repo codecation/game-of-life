75fd366e9b304c805fc1320237d40a8c169d4dca
Ben Orenstein <ben.orenstein@gmail.com>
2014-02-20 13:52:15 -0600

    Small refactorings

--- src-cljs/game_of_life/draw.cljs
+++ src-cljs/game_of_life/draw.cljs
@@ -5,22 +5,8 @@
 (def board-size (* cell-size 50))
 (def glider #{[1 1] [1 3] [2 3] [2 2] [3 2]})
 
-(defn- clear-board [context]
-  (.clearRect context 0 0 board-size board-size))
-
-(defn- draw-cell [x y context]
-  (.fillRect context (* cell-size x) (* cell-size y) cell-size cell-size))
-
-(defn- draw [living-cells context]
-  (clear-board context)
-  (doseq [[x y] living-cells]
-    (draw-cell x y context)))
-
-(defn- run [board context]
-  (draw board context)
-  (js/setTimeout (fn [] (run (game/advance board) context)) 100))
-
 (defn- make-context []
+  "Creates the native js context object"
   (let [canvas (.getElementById js/document "canvas")
         context (.getContext canvas "2d")]
     (set! (.-width canvas) board-size)
@@ -28,5 +14,22 @@
     (set! (.-fillStyle context) "rgb(0, 0, 0)")
     context))
 
+(defn- clear-board [context]
+  (.clearRect context 0 0 board-size board-size))
+
+(defn- draw-living-cell [x y context]
+  (.fillRect context (* cell-size x) (* cell-size y) cell-size cell-size))
+
+(defn- draw [living-cells context]
+  (clear-board context)
+  (doseq [[x y] living-cells]
+    (draw-living-cell x y context)))
+
+(defn- run [board context]
+  "Draws the board, sleeps, and recurs"
+  (draw board context)
+  (js/setTimeout (fn [] (run (game/advance board) context)) 100))
+
 (defn start []
+  "Start the simulation"
   (run glider (make-context)))
--- src-cljs/game_of_life/game.cljs
+++ src-cljs/game_of_life/game.cljs
@@ -1,18 +1,24 @@
 (ns game-of-life.game)
 
 (defn neighbors [[x y]]
+  "Returns the set of neighbors within one unit
+  of a given x y point"
   (set
     (for [dx [-1 0 1] dy [-1 0 1]
           :when (not= 0 dx dy)]
       [(+ dx x) (+ dy y)])))
 
 (defn should-live?
+  "Given a set of living cells and a frequency map of neighbors,
+  return the set of cells that survived."
   [living-cells [location occurrence-count]]
   (or (= occurrence-count 3)
       (and (= occurrence-count 2)
            (living-cells location))))
 
 (defn advance [living-cells]
+  "Given a set of living cells, advance one generation, returning
+  the set of cells that are now alive."
   (->>
     living-cells
     (mapcat neighbors)
