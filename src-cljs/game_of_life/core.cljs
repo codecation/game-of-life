810566a4d95b828fec3f89c97c8e70a6a43b20d7
Chris Hunt <chrishunt@github.com>
2014-02-20 12:14:12 -0600

    Houston, we have a glider

--- src-cljs/game_of_life/core.cljs
+++ src-cljs/game_of_life/core.cljs
@@ -21,8 +21,25 @@
     (map first)
     set))
 
-(defn start []
+(def cell-size 10)
+(def board-size (* cell-size 50))
+
+(defn draw [living-cells]
   (let [canvas (.getElementById js/document "canvas")
         context (.getContext canvas "2d")]
+    (set! (.-width canvas) board-size)
+    (set! (.-height canvas) board-size)
     (set! (.-fillStyle context) "rgb(0, 0, 0)")
-    (.fillRect context 10 10 10 10)))
+    (.clearRect context 0 0 board-size board-size)
+    (doseq [[x y] living-cells]
+      (.fillRect context (* cell-size x) (* cell-size y) cell-size cell-size))))
+
+(defn run [board]
+  (draw board)
+  (js/setTimeout (fn [] (run (advance board))) 100))
+
+(def glider #{[1 1] [1 3] [2 3] [2 2] [3 2]})
+
+(defn start []
+  (run glider))
+
